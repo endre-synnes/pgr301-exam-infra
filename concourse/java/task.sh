7cfb7a0a9e6b754dd0313d358cf177ba6e9b2621
Endre Synnes <synend16@student.westerdals.no>
2018-11-22 11:07:35 +0100

    updated for chacking of maven dependensies

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -9,3 +9,13 @@ export M2_LOCAL_REPO=".m2"
 
 mvn -f source/pom.xml install 
 echo -e "${GREEN}${CHECK} Maven install${NC}"
+
+export ROOT_FOLDER=$( pwd )
+export REPO=repo
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
