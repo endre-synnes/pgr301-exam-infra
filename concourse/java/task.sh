5bc718f1c5e73d31d51d7a24b36a0b8a0f25e2d8
Endre Synnes <synend16@student.westerdals.no>
2018-11-22 11:22:33 +0100

    done some updates to shell script in task.sh

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -10,10 +10,27 @@ export M2_LOCAL_REPO=".m2"
 mvn -f source/pom.xml install 
 echo -e "${GREEN}${CHECK} Maven install${NC}"
 
-export ROOT_FOLDER=$( pwd )
-export REPO=repo
 
-M2_HOME="${HOME}/.m2"
+
+export ROOT_FOLDER=$( pwd )
+M2_HOME=${HOME}/.m2
+mkdir -p ${M2_HOME}
+
+M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
+
+mkdir -p "${M2_LOCAL_REPO}/repository"
+
+cat > ${M2_HOME}/settings.xml <<EOF
+
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
+</settings>
+
+EOF
+
 M2_CACHE="${ROOT_FOLDER}/maven"
 
 echo "Generating symbolic links for caches"
