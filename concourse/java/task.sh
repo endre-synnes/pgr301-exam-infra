5f2f44a660094087016deb907ced5292ea09c174
Endre Synnes <synend16@student.westerdals.no>
2018-11-22 13:37:24 +0100

    updated task sh

--- concourse/java/task.sh
+++ concourse/java/task.sh
@@ -8,30 +8,24 @@ export CHECK="√"
 export M2_LOCAL_REPO=".m2"
 
 
+# START Caching
 export ROOT_FOLDER=$( pwd )
-M2_HOME="${ROOT_FOLDER}/.m2"
+M2_HOME=${HOME}/.m2
 mkdir -p ${M2_HOME}
 
-mkdir -p "${M2_HOME}/repository"
+M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
+
+mkdir -p "${M2_LOCAL_REPO}/repository"
 
 cat > ${M2_HOME}/settings.xml <<EOF
-
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${M2_HOME}/repository</localRepository>
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                         https://maven.apache.org/xsd/settings-1.0.0.xsd">
+     <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
 </settings>
-
 EOF
-
-M2_CACHE="${ROOT_FOLDER}/maven"
-
-echo "Generating symbolic links for caches"
-
-[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
-
+# END Caching
 
 mvn -f source/pom.xml install
 echo -e "${GREEN}${CHECK} Maven install${NC}"
-
